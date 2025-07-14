dec8f8796afc861349a035a7d8494085207e96b7
Manodhya Opallage <48380231+iNVISIBLExtanx@users.noreply.github.com>
2025-07-13 23:20:16 -0400

    fix: upgrade chatscreen page

--- chat-live-location-app/src/screens/ChatScreen.tsx
+++ chat-live-location-app/src/screens/ChatScreen.tsx
@@ -76,7 +76,7 @@ const ChatScreen: React.FC = () => {
               if (connection) {
                 Alert.alert(
                   'Connection Request Sent',
-                  `Receiver ${receiverName} will be notified of the update of the receiver`
+                  `Receiver ${receiverName} will be notified of the update of the`
                 );
               } else {
                 Alert.alert(
